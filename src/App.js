670bc5ea5e0dd1a980f3e8d35094b1466e9f95f1
jeanknieling <jeanknieling@gmail.com>
2022-02-28 21:34:35 -0300

    style: Estilos globais implementados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Routes from './Routes';
+import GlobalStyled from './styles/global';
 
 const App = () => {
     return (
         <>
+            <GlobalStyled/>
+
             <Routes/>
         </>
     );
---
+++ src/styles/global.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    :root {
+        --color-primary: #FF577F;
+        --color-primary-focus: #FF427F;
+        --color-primary-negative: #59323F;
+
+        --gray-0: #F8F9FA;
+        --gray-1: #868E96;
+        --gray-2: #343B41;
+        --gray-3: #212529;
+        --gray-4: #121214;
+
+        --sucess: #3FE864;
+        --error: #E83F5B;
+    }
+
+    body {
+        background-color: var(--gray-4);
+        color: var(--gray-0);
+    }
+
+    body, input, button {
+        font-family: 'Inter', Helvetica, sans-serif;
+        font-size: 1rem;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-weight: 700;
+    }
+
+    button, input {
+        cursor: pointer;
+    }
+
+    a { 
+        text-decoration: none;
+    }
+
+`;
